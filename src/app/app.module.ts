af7f3c67aec0e77be0b867bb554bed721ba702df
Sofiane Lasri-Trienpont <alasri250@gmail.com>
2024-11-26 12:25:04 +0100

    feat: add index component and update routing
    - Created `index.component.html` with basic structure.
    - Added `IndexComponent` to the application with associated spec and styles.
    - Updated `app-routing.module.ts` to include the IndexComponent as the default route.
    - Removed placeholder content from `app.component.html` and replaced it with a `<main>` tag.
    - Introduced `media-breakpoints.scss` for responsive design and included it in `styles.scss`.
    - Imported `MatSlideToggleModule` in `app.module.ts` and registered `IndexComponent`.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatSlideToggleModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
