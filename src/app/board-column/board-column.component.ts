28729c5d1402f43930887acd7483370369611cb7
SofianeLasri <alasri250@gmail.com>
2024-11-27 16:23:57 +0100

    feat(board-column): integrate BoardTask component
    - Added the `BoardTaskComponent` to the `board-column.component.ts` and updated the template to include `<app-board-task />`.
    - Prevented text selection on the `title` class in `board-column.component.scss` for better UX.
    - Introduced `BoardTaskComponent` with new template, styling, and spec files.
    - Updated `header.component.scss` to ensure text within headers is not selectable.

--- src/app/board-column/board-column.component.ts
+++ src/app/board-column/board-column.component.ts
@@ -1,8 +1,12 @@
 import {Component, Input} from '@angular/core';
+import {BoardTaskComponent} from "../board-task/board-task.component";
 
 @Component({
   selector: 'app-board-column',
   templateUrl: './board-column.component.html',
+    imports: [
+        BoardTaskComponent
+    ],
   styleUrls: ['./board-column.component.scss']
 })
 export class BoardColumnComponent {
---
+++ src/app/board-task/board-task.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-board-task',
+  imports: [],
+  templateUrl: './board-task.component.html',
+  styleUrl: './board-task.component.scss'
+})
+export class BoardTaskComponent {
+
+}
