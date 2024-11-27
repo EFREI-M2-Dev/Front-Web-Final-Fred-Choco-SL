12d368e0529a5f6e3ac3203f2cff336de3ef801e
Fredray-21 <dabadie.frederic@gmail.com>
2024-11-27 14:52:44 +0100

    feat(navBar) : style home/notHome && useSessionUser

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input} from '@angular/core';
+import {Observable} from "rxjs";
+import {AuthService} from "../Auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -8,4 +11,25 @@ import {Component, Input} from '@angular/core';
 })
 export class NavbarComponent {
   @Input() isHomePage: boolean = false;
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.isLoggedIn$ = this.authService.isLoggedIn();
+  }
+
+  handleLogout() {
+    this.authService.logout();
+  }
+
+  async goToHome() {
+    await this.router.navigate(['/']);
+  }
+
+  async goToLogin() {
+    await this.router.navigate(['/login']);
+  }
+
+  async goToRegister() {
+    await this.router.navigate(['/register']);
+  }
 }
