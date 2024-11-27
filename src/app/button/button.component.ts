f55e9055aca950b2d99bd6bf2cae0d86dcbb95c7
SofianeLasri <alasri250@gmail.com>
2024-11-27 11:50:16 +0100

    feat: update button styles and index layout
    - Updated SCSS to replace `.mat-secondary` with `.mat-tonal` for enhanced button styling.
    - Modified TypeScript input property in `button.component.ts` to include 'tonal' as a color option.
    - Enhanced `index.component.html` by adding structured headline and action sections for improved layout.
    - Updated `index.component.scss` to use a flexbox layout, introducing new styles for headlines and actions.
    - Altered `navbar.component.html` to utilize the new 'tonal' color for buttons, reflecting the updated design.
    - Applied a z-index of 10 to `navbar.component.scss` for proper layering over content.

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -8,7 +8,7 @@ import {Component, Input} from '@angular/core';
 })
 export class ButtonComponent {
   @Input() label: string = 'Bouton'; // Texte du bouton
-  @Input() color: 'primary' | 'secondary' = 'primary'; // Couleur
+  @Input() color: 'primary' | 'secondary' | 'tonal' = 'primary'; // Couleur
   @Input() text: 'primary' | 'secondary' = 'secondary';
   @Input() type: 'basic' | 'raised' | 'stroked' | 'flat' | 'icon' = 'basic'; // Type de bouton
   @Input() disabled: boolean = false; // Désactiver le bouton
