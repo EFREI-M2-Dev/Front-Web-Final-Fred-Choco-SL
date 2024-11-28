19fec731d668431732a74c35dcc17890d25631df
SofianeLasri <alasri250@gmail.com>
2024-11-28 15:20:08 +0100

    Revert "feat(material-ui): integrate Angular Material components for improved UI consistency"
    This reverts commit df44c26a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,8 +29,6 @@ import {BoardTaskComponent} from "./board-task/board-task.component";
 import {ConfirmDeleteModalComponent} from "./confirm-delete-modal/confirm-delete-modal.component";
 import {AddTaskModalComponent} from "./add-task-modal/add-task-modal.component";
 import {DragDropModule} from "@angular/cdk/drag-drop";
-import {MatError, MatFormField} from "@angular/material/form-field";
-import {MatInput, MatLabel} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -63,10 +61,6 @@ import {MatInput, MatLabel} from "@angular/material/input";
     AngularSvgIconModule.forRoot(),
     MatTabsModule,
     MatIconModule,
-    MatFormField,
-    MatInput,
-    MatError,
-    MatLabel,
     DragDropModule
   ],
   providers: [
